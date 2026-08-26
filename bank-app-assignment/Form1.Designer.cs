cb5e5a524d30b0e2f5485d5f635e9bf3038c8136
mcrane-0 <263767342+mcrane-0@users.noreply.github.com>
2026-08-26 14:45:22 -0400

    changed button color

--- bank-app-assignment/Form1.Designer.cs
+++ bank-app-assignment/Form1.Designer.cs
@@ -39,12 +39,13 @@
             // 
             // btnCreateAccount
             // 
-            btnCreateAccount.BackColor = Color.FromArgb(128, 128, 255);
+            btnCreateAccount.BackColor = SystemColors.Control;
             btnCreateAccount.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
             btnCreateAccount.ForeColor = Color.Green;
-            btnCreateAccount.Location = new Point(280, 314);
+            btnCreateAccount.Location = new Point(245, 236);
+            btnCreateAccount.Margin = new Padding(3, 2, 3, 2);
             btnCreateAccount.Name = "btnCreateAccount";
-            btnCreateAccount.Size = new Size(230, 73);
+            btnCreateAccount.Size = new Size(201, 55);
             btnCreateAccount.TabIndex = 0;
             btnCreateAccount.Text = "Create Account";
             btnCreateAccount.UseVisualStyleBackColor = false;
@@ -52,56 +53,59 @@
             // lblName
             // 
             lblName.AutoSize = true;
-            lblName.Location = new Point(116, 25);
+            lblName.Location = new Point(102, 19);
             lblName.Name = "lblName";
-            lblName.Size = new Size(52, 20);
+            lblName.Size = new Size(42, 15);
             lblName.TabIndex = 1;
             lblName.Text = "Name:";
             // 
             // lblNumber
             // 
             lblNumber.AutoSize = true;
-            lblNumber.Location = new Point(467, 25);
+            lblNumber.Location = new Point(409, 19);
             lblNumber.Name = "lblNumber";
-            lblNumber.Size = new Size(63, 20);
+            lblNumber.Size = new Size(51, 15);
             lblNumber.TabIndex = 2;
             lblNumber.Text = "Number";
             // 
             // lblBalance
             // 
             lblBalance.AutoSize = true;
-            lblBalance.Location = new Point(116, 112);
+            lblBalance.Location = new Point(102, 84);
             lblBalance.Name = "lblBalance";
-            lblBalance.Size = new Size(105, 20);
+            lblBalance.Size = new Size(83, 15);
             lblBalance.TabIndex = 3;
             lblBalance.Text = "Initial Balance:";
             // 
             // txtNumber
             // 
-            txtNumber.Location = new Point(467, 48);
+            txtNumber.Location = new Point(409, 36);
+            txtNumber.Margin = new Padding(3, 2, 3, 2);
             txtNumber.Name = "txtNumber";
-            txtNumber.Size = new Size(190, 27);
+            txtNumber.Size = new Size(167, 23);
             txtNumber.TabIndex = 4;
             // 
             // txtBalance
             // 
-            txtBalance.Location = new Point(116, 151);
+            txtBalance.Location = new Point(102, 113);
+            txtBalance.Margin = new Padding(3, 2, 3, 2);
             txtBalance.Name = "txtBalance";
-            txtBalance.Size = new Size(196, 27);
+            txtBalance.Size = new Size(172, 23);
             txtBalance.TabIndex = 5;
             // 
             // txtName
             // 
-            txtName.Location = new Point(116, 48);
+            txtName.Location = new Point(102, 36);
+            txtName.Margin = new Padding(3, 2, 3, 2);
             txtName.Name = "txtName";
-            txtName.Size = new Size(200, 27);
+            txtName.Size = new Size(176, 23);
             txtName.TabIndex = 6;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(700, 338);
             Controls.Add(txtName);
             Controls.Add(txtBalance);
             Controls.Add(txtNumber);
@@ -109,6 +113,7 @@
             Controls.Add(lblNumber);
             Controls.Add(lblName);
             Controls.Add(btnCreateAccount);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
--- bank-app-assignment/obj/Debug/net10.0-windows/bank-app-assignment.AssemblyInfo.cs
+++ bank-app-assignment/obj/Debug/net10.0-windows/bank-app-assignment.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bank-app-assignment")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c747dcc55f9dcd4c13d3573daefe255a174244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed706c42b653218698ac583f9e536e3189ad5d83")]
 [assembly: System.Reflection.AssemblyProductAttribute("bank-app-assignment")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bank-app-assignment")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
