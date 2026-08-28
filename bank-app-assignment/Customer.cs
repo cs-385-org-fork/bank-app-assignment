4bbe6979a0dd147818a709db51f3e3fbed2d99f4
Trevor Past <trevorpastfb@gmail.com>
2026-08-28 18:17:38 -0400

    getter and setter fixed

--- bank-app-assignment/Customer.cs
+++ bank-app-assignment/Customer.cs
@@ -8,11 +8,83 @@ namespace bank_app_assignment
 {
     internal class Customer
     {
-      public int AccountNumber { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public double Balance { get; set; }
+        private int _accountNumber;
+        private string _firstName;
+        private string _lastName;
+        private double _balance;
+        public int AccountNumber
+        {
+            get
+            {
+                return _accountNumber;
+            }
+            set
+            {
+                _accountNumber = value;
+            }
+        }
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+        public double Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
 
+       /* public int GetAccountNumber() 
+        {
+            int account = AccountNumber; 
+            return account;
+        }
+        public void SetAccountNumber(int account)
+        {
+            AccountNumber = account;
+        }
+        public string GetFirstName()
+        {
+            string fName = FirstName;
+            return fName;
+        }
+        public void SetFirstName(string fName)
+        {
+            FirstName = fName;
+        }
+        public string GetLastName()
+        {
+            string lName = FirstName;
+            return lName;
+        }
+        public void SetLastName(string lName)
+        {
+            LastName = lName;
+        }
+       */
         public double DepositTo(int account, double amount)
         {
             int accountID = account;
