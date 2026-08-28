90fd9d02f3b3164b10840141acefa48e2c590d96
Trevor Past <trevorpastfb@gmail.com>
2026-08-28 17:34:21 -0400

    methods added

--- bank-app-assignment/Customer.cs
+++ bank-app-assignment/Customer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Imaging.Effects;
+using System.Security.Principal;
 using System.Text;
 
 namespace bank_app_assignment
@@ -9,14 +11,36 @@ namespace bank_app_assignment
         private int accountNumber { get; set; }
         private string firstName { get; set; }
         private string lastName { get; set; }
-        private double initialBalance { get; set; }
+        private double balance { get; set; }
 
+        public double depositTo(int account, double amount)
+        {
+            int accountID = account;
+            double amountAdded = amount;
+            if (accountID == accountNumber)
+            {
+                balance += amountAdded;
+
+            }
+            return balance;
+        }
+        public double withdrawFrom(int account, double amount)
+        {
+            int accountID = account;
+            double amountWithdrawn = amount;
+            if (accountID == accountNumber)
+            {
+                balance -= amountWithdrawn;
+            }
+            return balance;
+        }
         public Customer()
         {
             accountNumber = 0;
             firstName = "";
             lastName = "";
-            initialBalance = 0.0;
+            balance = 0.0;
         }
+        
     }
 }
