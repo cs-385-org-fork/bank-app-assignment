c9b1b2e77a369139051b4f59b3548ffd1476fd55
mcrane-0 <263767342+mcrane-0@users.noreply.github.com>
2026-08-28 18:29:31 -0400

    more event stuff

--- bank-app-assignment/Form1.cs
+++ bank-app-assignment/Form1.cs
@@ -7,12 +7,31 @@ namespace bank_app_assignment
             InitializeComponent();
         }
 
+        private Customer cust = new Customer();
+
         private void btnCreateAccount_Click(object sender, EventArgs e)
         {
-            Customer cust = new Customer();
-
             int nameBreak = txtName.Text.IndexOf(" ");
-            cust.FirstName.set = "eabure";
+            cust.FirstName = txtName.Text.Substring(0, nameBreak);
+            cust.LastName = txtName.Text.Substring(nameBreak + 1, txtName.Text.Length - nameBreak - 1);
+            cust.AccountNumber = Int32.Parse(txtNumber.Text);
+            cust.Balance = Double.Parse(txtBalance.Text);
+
+            lblCurrentBalance.Text = ("Current Balance: $" + cust.Balance);
+        }
+
+        private void btnDeposit_Click(object sender, EventArgs e)
+        {
+            cust.DepositTo(Int32.Parse(txtNumber.Text), Double.Parse(txtDeposit.Text));
+            lblCurrentBalance.Text = ("Current Balance: $" + cust.Balance);
+            Console.WriteLine(cust.Balance);
+        }
+
+        private void btnWithdraw_Click(object sender, EventArgs e)
+        {
+            cust.WithdrawFrom(Int32.Parse(txtNumber.Text), Double.Parse(txtWithdraw.Text));
+            lblCurrentBalance.Text = ("Current Balance: $" + cust.Balance);
+            Console.WriteLine(cust.Balance);
         }
     }
 }
